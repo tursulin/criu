ad2988c7c1610eca0c6ddf257be9adab8d133dd8
Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
2026-02-13 14:05:32 +0000

    plugins/amdgpu: Fix drm pages size header
    Size needs to be initialized before opening the file for write, otherwise
    garbage is written into the file.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
    Reviewed-By: David Francis <David.Francis@amd.com>

--- plugins/amdgpu/amdgpu_plugin_drm.c
+++ plugins/amdgpu/amdgpu_plugin_drm.c
@@ -378,6 +378,7 @@ int amdgpu_plugin_drm_dump_file(int fd, int id, struct stat *drm)
 		drmPrimeHandleToFD(device_fd, boinfo->handle, 0, &dmabuf_fd);
 
 		snprintf(img_path, sizeof(img_path), IMG_DRM_PAGES_FILE, rd->id, rd->drm_render_minor, i);
+		image_size = handle_entry.size;
 		bo_contents_fp = open_img_file(img_path, true, &image_size);
 
 		posix_memalign(&buffer, sysconf(_SC_PAGE_SIZE), handle_entry.size);
