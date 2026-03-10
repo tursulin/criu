8c5311e004a7d2d159ca9f4174cda9850a4636a0
Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
2026-03-10 16:20:40 +0000

    plugins/amdgpu: Fix open_drm_render_device()
    Open_drm_render_device() is a little bit confused. It can log confusing
    errors using errno which hasn't been set, or it has been overwritten. It
    can also inspect errno after it had reset it creating effectively
    unreachable code. And finally it sometimes returns the negative errno, and
    sometimes a plain -1. Fix it all up and make it consistent, while at the
    same time correcting two calls site incorrectly using pr_perror(), while
    the function does not guarantee a valid errno.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>

--- plugins/amdgpu/amdgpu_plugin.c
+++ plugins/amdgpu/amdgpu_plugin.c
@@ -1597,7 +1597,8 @@ static int restore_devices(struct kfd_ioctl_criu_args *args, CriuKfd *e)
 
 		drm_fd = node_get_drm_render_device(tp_node);
 		if (drm_fd < 0) {
-			pr_perror("Can't open drm render fd %d", tp_node->drm_render_minor);
+			pr_err("Can't open drm render fd for minor %d - %s",
+			       tp_node->drm_render_minor, strerror(-drm_fd));
 			goto exit;
 		} else {
 			pr_info("passing drm render fd = %d to driver\n", drm_fd);
@@ -1905,7 +1906,8 @@ int amdgpu_plugin_restore_file(int id, bool *retry_needed)
 
 		fd = node_get_drm_render_device(tp_node);
 		if (fd < 0) {
-			pr_err("Failed to open render device (minor:%d)\n", tp_node->drm_render_minor);
+			pr_err("Failed to open render device (minor:%d) - %s\n",
+			       tp_node->drm_render_minor, strerror(-fd));
 			return -1;
 		}
 
@@ -2199,9 +2201,8 @@ int init_dev(int dev_minor, amdgpu_device_handle *h_dev, uint64_t *max_copy_size
 	struct amdgpu_gpu_info gpu_info = { 0 };
 
 	drm_fd = open_drm_render_device(dev_minor);
-	if (drm_fd < 0) {
+	if (drm_fd < 0)
 		return drm_fd;
-	}
 
 	ret = amdgpu_device_initialize(drm_fd, &major, &minor, h_dev);
 	if (ret) {
