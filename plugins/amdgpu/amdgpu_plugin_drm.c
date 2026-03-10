50ee105d1e7cf9400899e83b1b8a64a7a2da91f8
Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
2026-03-10 15:32:31 +0000

    plugins/amdgpu: Fix remaining wrong usages of pr_perror
    Mostly after calling libdrm functions pr_perror is wrong because calling
    convention there is to return zero or negative error code - errno is not
    guaranteed to be valid. So convert those to pr_err with manually calling
    strerror to log user friendly messages. Same conversion for handling
    errors from some plugin internal functions.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>

--- plugins/amdgpu/amdgpu_plugin_drm.c
+++ plugins/amdgpu/amdgpu_plugin_drm.c
@@ -137,9 +137,9 @@ int amdgpu_plugin_drm_handle_device_vma(int fd, const struct stat *st)
 
 	snprintf(path, sizeof(path), AMDGPU_DRM_DEVICE, DRM_FIRST_RENDER_NODE);
 	ret = stat(path, &drm);
-	if (ret == -1) {
+	if (ret) {
 		pr_err("Error in getting stat for: %s\n", path);
-		return ret;
+		return -errno;
 	}
 
 	if ((major(st->st_rdev) != major(drm.st_rdev)) ||
@@ -170,13 +170,14 @@ static int restore_bo_contents_drm(int drm_render_minor, CriuRenderNode *rd, int
 
 	ret = amdgpu_device_initialize(drm_fd, &major, &minor, &h_dev);
 	if (ret) {
-		pr_perror("failed to initialize device");
+		pr_err("failed to initialize device - %s", strerror(-ret));
 		goto exit;
 	}
 
 	ret = amdgpu_query_gpu_info(h_dev, &gpu_info);
 	if (ret) {
-		pr_perror("failed to query gpuinfo via libdrm");
+		pr_err("failed to query gpuinfo via libdrm - %s",
+		       strerror(-ret));
 		goto exit;
 	}
 
